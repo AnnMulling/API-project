6627d333ac7e5934e2f9b58d9e536d053d9beaf7
AnnPrim <Primpraowann@gmail.com>
2023-09-27 17:46:58 -0500

    Add error-handling middleware for common errors and debugging

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,25 @@
-// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
 
-module.exports = router;
+  router.use('/api', apiRouter);
+  module.exports = router;
+
+//   fetch('/api/test', {
+//     method: "POST",
+//     headers: {
+//         "Content-Type" : "application/json",
+//         "XSRF-TOKEN": "h4jNQc3J-leXQUSuEfwtkv7QzCgraB0e1Ug0"
+//     },
+//     body: JSON.stringify({hello: 'world'})
+//   }).then(res => res.json()).then(data => console.log(data))
+//"EqqJXjyX--dJVFr9q82DusVOc8O3TQcAmMVo"
