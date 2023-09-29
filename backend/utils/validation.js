e9eb1e905e2d7465b65665876ed2a856d2c448db
AnnPrim <Primpraowann@gmail.com>
2023-09-29 10:13:12 -0500

    Add User input validation on user login backend endpoint

---
+++ backend/utils/validation.js
@@ -0,0 +1,29 @@
+//Validating the Request Body
+
+const { validationResult } = require('express-validator');
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+    //calling validationResult to validate request
+    const validationErrors = validationResult(req);
+    // if there are validation errors, create an error with all error message
+    if (!validationErrors.isEmpty()) {
+        const errors = {};
+        validationErrors
+            .array()
+            .forEach(error => errors[error.path] = error.msg);
+
+        const err = Error("Bad request.");
+        err.errors = errors;
+        err.status = 400;
+        err.title = "Bad request.";
+        next(err);
+    }
+    // if no error, invoke the next error-handling middleware
+    next();
+};
+
+module.exports = {
+    handleValidationErrors
+};
