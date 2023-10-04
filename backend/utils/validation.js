3f001caffefad157412fb3310c89b55e2471f405
AnnPrim <Primpraowann@gmail.com>
2023-10-04 09:41:02 -0500

    unscoped editing spot

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -14,10 +14,10 @@ const handleValidationErrors = (req, _res, next) => {
             .array()
             .forEach(error => errors[error.path] = error.msg);
 
-        const err = Error("Bad request.");
+        const err = Error("Bad Request");
         err.errors = errors;
         err.status = 400;
-        err.title = "Bad request.";
+        err.title = "Bad Request";
         next(err);
     }
     // if no error, invoke the next error-handling middleware
