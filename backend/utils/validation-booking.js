8468df5c65ea51eefae8efe0b2fb5146c7a7bd1a
AnnPrim <Primpraowann@gmail.com>
2023-10-08 21:23:12 -0500

    refactored validation-booking

--- backend/utils/validation-booking.js
+++ backend/utils/validation-booking.js
@@ -51,50 +51,35 @@ const dateExistsCreate = async function (req, _res, next) {
     if (bookings.length) {
        for (let booking of bookings) {
 
-        const oldStart = new Date(booking.startDate).getTime();
-        const oldEnd = new Date(booking.endDate).getTime();
+        const oldStartDate = new Date(booking.startDate).getTime();
+        const oldEndDate = new Date(booking.endDate).getTime();
 
-        const newStart = new Date(startDate).getTime();
-        const newEnd = new Date(endDate).getTime();
-       //StartDate
-           //if the existing startDate is after the desired startDate AND before the desired endDate
-           if ( (oldStart > newStart && oldStart < newEnd) ||
-           //OR the existing startDateate is the same as the desired startDateate
-                 (oldStart === newStart) ||
-                 //OR the Desired newStart is before the exisiting newStart AND before the exisitng endDate
-                 (oldStart < newStart && oldEnd > newStart) ||
-                 //OR the existing endDate is the same as the desire newStart
-                 (oldEnd === newStart)
-           ){
-                   error.errors.newStart = "Start date conflicts with an existing booking"
-                   sendMessage = true
+        const newStartDate = new Date(startDate).getTime();
+        const newEndDate = new Date(endDate).getTime();
+
+        //StartDate
+            if(newStartDate >= oldStartDate && newStartDate <= oldEndDate){
+                error.errors.startDate = "Start date conflicts with an existing booking"
+                sendMessage = true;
             }
 
        //EndDate
-       //if the existing newStart is same as endDate
-            if ((oldStart === newEnd) ||
-            //OR if the existing newStart is after the desired newEnd AND the existing newEnd is after the disired newEnd
-                (oldStart < newEnd && oldEnd > newEnd) ||
-                //OR if the existing endtDate is same as newEnd
-                (oldEnd === newEnd) ||
-                //OR if  the desired newStart is before the existing newEnd AND the existing newEnd is before the desired newEnd
-                (oldEnd > newStart &&  oldEnd < newEnd)
-            ) {
-                   error.errors.newEnd = "End date conflicts with an existing booking"
-                   sendMessage = true
-               };
+            if(newEndDate >= oldStartDate && newEndDate <= oldEndDate) {
+                error.errors.endDate = "End date conflicts with an existing booking"
+                sendMessage = true;
+            }
+
        //Both
-       // if the newStart is after desired newStart AND the existing newEnd is before the desired newEnd
-            if (oldStart > newStart && oldEnd < newEnd ) {
-                   error.errors.newEnd = "End date conflicts with an existing booking";
-                   error.errors.newStart = "Start date conflicts with an existing booking"
-                   sendMessage = true;
-            };
+            if(newStartDate < oldStartDate && oldEndDate < newEndDate) {
+                error.errors.endDate = "End date conflicts with an existing booking";
+                error.errors.startDate = "Start date conflicts with an existing booking"
+                sendMessage = true;
+             }
 
             if (sendMessage) {
-                   error.message = "Sorry, this spot is already booked for the specified dates"
-                   error.status = 403;
-                   next(error)
+                error.message = "Sorry, this spot is already booked for the specified dates"
+                error.status = 403;
+                next(error);
             }
        }
     }
@@ -137,23 +122,25 @@ const dateExistsEdit = async function(req, res, next) {
         };
 
         if(newStartDate >= oldStartDate && newStartDate <= oldEndDate){
-            error.errors.newStart = "Start date conflicts with an existing booking"
+            error.errors.startDate = "Start date conflicts with an existing booking"
             sendMessage = true;
         }
+
         if(newEndDate >= oldStartDate && newEndDate <= oldEndDate) {
-            error.errors.newEnd = "End date conflicts with an existing booking"
+            error.errors.endDate = "End date conflicts with an existing booking"
             sendMessage = true;
         }
+
         if(newStartDate < oldStartDate && oldEndDate < newEndDate) {
-            error.errors.newEnd = "End date conflicts with an existing booking";
-            error.errors.newStart = "Start date conflicts with an existing booking"
+            error.errors.endDate = "End date conflicts with an existing booking";
+            error.errors.startDate = "Start date conflicts with an existing booking"
             sendMessage = true;
         }
         if (sendMessage) {
             error.message = "Sorry, this spot is already booked for the specified dates"
             error.status = 403;
-            next(error)
-     }
+            next(error);
+        }
 
     });
 
