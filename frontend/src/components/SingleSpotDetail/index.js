952c2313e0a7bbe2c8edcda402652b432144a79d
AnnPrim <Primpraowann@gmail.com>
2023-10-22 22:52:43 -0500

    spotTile and description on home page completed

---
+++ frontend/src/components/SingleSpotDetail/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function SpotDetail () {
+    return (
+        <>
+            <h1>hello from spot details</h1>
+        </>
+    );
+};
+
+
+export default SpotDetail;
